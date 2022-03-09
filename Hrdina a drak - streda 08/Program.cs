5ebeb0c4ebdb5fc8d673f34a8fc229b6d570e502
psc-utb <psc.utb@gmail.com>
2022-03-09 08:52:05 +0100

    Kopírování objektu

--- Hrdina a drak - streda 08/Program.cs
+++ Hrdina a drak - streda 08/Program.cs
@@ -6,9 +6,20 @@ namespace Hrdina_a_drak___streda_08
     {
         static void Main(string[] args)
         {
-            Hrdina hrdina = new Hrdina("Geralt", 100, 100, 10, 10, null);
+            Mec mec = new Mec(20);
+            Hrdina hrdina = new Hrdina("Geralt", 100, 100, 10, 10, mec);
             Drak drak = new Drak("Alduin", 100, 100, 11, 10);
             Vlk vlk = new Vlk("Wolf", 50, 50, 5, 5);
+
+            Hrdina klon = hrdina.Clone();
+            klon.Jmeno += " (klon)";
+            klon.Zdravi = 200;
+            klon.Utekl = true;
+            klon.PoskozeniMax = 15;
+            klon.Mec.PoskozeniMax = 50;
+            Console.WriteLine(hrdina.ToString());
+            Console.WriteLine(klon.ToString());
+            Console.WriteLine(String.Empty);
 
             /*Arena arena = new Arena(hrdina, drak);
             arena.Boj();*/
